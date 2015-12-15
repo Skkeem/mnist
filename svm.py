6c8846213da4e0c39308f2f2d037b05c5f05f379
David Yang <david.yang@mentisware.com>
2015-12-16 04:13:22 +0900

    Update svm.py

--- svm.py
+++ svm.py
@@ -6,16 +6,13 @@ I use the well known optimization solver library, cvxopt(http://cvxopt.org)"""
 import cvxopt
 import cvxopt.solvers
 
-
-import theano
-import theano.tensor as T
 import input_data
 
 
 def linear_kernel(x1, x2):
     return np.dot(x1, x2)
 
-def polynomial_kernel(x1, x2, p=4):
+def polynomial_kernel(x1, x2, p=3):
     return (1 + np.dot(x1, x2)) ** p
 
 def gaussian_kernel(x1, x2, sigma=5.0):
@@ -103,6 +100,25 @@ class SVM(object):
     def predict(self, X):
         return np.sign(self.project(X))
 
+    def margin(self, X):
+        # get the distance to the separator
+        if self.w is not None:
+            return (np.dot(X, self.w) + self.b) / linalg.norm(self.w)
+        else:
+            y_pred = np.zeros(len(X))
+            for i in range(len(X)):
+                s = 0
+                for a, sv_y, sv in zip(self.a, self.sv_y, self.sv):
+                    s += a * sv_y * self.kernel(X[i], sv)
+                y_pred[i] = s
+
+            # it's a bit difficult to calculate the precise value of ||w||.
+            # instead use the similar equation as linear case
+            weight = np.zeros(X.shape[1])
+            for a, sv_y, sv in zip(self.a, self.sv_y, self.sv):
+                weight += a * sv_y * sv
+            return (y_pred + self.b) / linalg.norm(weight)
+                    
 
 """
 The classifier should classify a number between 0 to 9.
@@ -122,7 +138,7 @@ class DigitSVM(object):
         SVMs = []
         for i in range(0, 10):
             print "...Training for number %d" % ( i )
-            svm = SVM()
+            svm = SVM(kernel=self.kernel, C=self.C)
             #svm.fit(X, map(lambda l: np.float64(1) if l == i else np.float64(-1), y))
             svm.fit(X, np.array(map(lambda l: 1. if l == i else -1., y)))
             SVMs.append( svm )
@@ -135,7 +151,7 @@ class DigitSVM(object):
         max_margin = np.zeros( X.shape[0] )
         num = np.zeros( X.shape[0], dtype=np.int8 )
         for i in range(0, 10):
-            margin = self.SVMs[i].project(X)
+            margin = self.SVMs[i].margin(X)
             for j in range(len(margin)):
                 if margin[j] > max_margin[j]:
                     max_margin[j] = margin[j]
@@ -162,18 +178,132 @@ if __name__ == "__main__":
     #print datasets.validation.labels.shape
     #print datasets.validation.labels
 
-    SIZE = 5000
+    """
+    SIZE = 1000
     train_images = datasets.train.images[:SIZE]
     train_labels = datasets.train.labels[:SIZE]
 
     test_images = datasets.test.images[:1000]
     test_labels = datasets.test.labels[:1000]
+    """
 
+    train_images = datasets.train.images
+    train_labels = datasets.train.labels
 
-    digitSVM = DigitSVM()
+    test_images = datasets.test.images
+    test_labels = datasets.test.labels
+
+    digitSVM = DigitSVM(kernel=gaussian_kernel)
     digitSVM.fit(train_images, train_labels)
 
     y_predict = digitSVM.predict(test_images)
     correct = np.sum(y_predict == test_labels)
     print "%d out of %d predictions correct" % (correct, len(y_predict))
 
+
+    """
+    def test_linear():
+        X1, y1, X2, y2 = gen_lin_separable_data()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+        clf = SVM()
+        clf.fit(X_train, y_train)
+
+        y_predict = clf.predict(X_test)
+        correct = np.sum(y_predict == y_test)
+        print "%d out of %d predictions correct" % (correct, len(y_predict))
+
+        # plot_margin(X_train[y_train==1], X_train[y_train==-1], clf)
+
+    def test_non_linear():
+        X1, y1, X2, y2 = gen_non_lin_separable_data()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+        clf = SVM(gaussian_kernel)
+        clf.fit(X_train, y_train)
+
+        y_predict = clf.predict(X_test)
+        correct = np.sum(y_predict == y_test)
+        print "%d out of %d predictions correct" % (correct, len(y_predict))
+
+        # plot_contour(X_train[y_train==1], X_train[y_train==-1], clf)
+
+    def test_soft():
+        X1, y1, X2, y2 = gen_lin_separable_overlap_data()
+        X_train, y_train = split_train(X1, y1, X2, y2)
+        X_test, y_test = split_test(X1, y1, X2, y2)
+
+        clf = SVM(C=0.1)
+        clf.fit(X_train, y_train)
+
+        y_predict = clf.predict(X_test)
+        correct = np.sum(y_predict == y_test)
+        print "%d out of %d predictions correct" % (correct, len(y_predict))
+
+
+    def gen_lin_separable_data():
+        # generate training data in the 2-d case
+        mean1 = np.array([0, 2])
+        mean2 = np.array([2, 0])
+        cov = np.array([[0.8, 0.6], [0.6, 0.8]])
+        X1 = np.random.multivariate_normal(mean1, cov, 100)
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 100)
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def gen_non_lin_separable_data():
+        mean1 = [-1, 2]
+        mean2 = [1, -1]
+        mean3 = [4, -4]
+        mean4 = [-4, 4]
+        cov = [[1.0,0.8], [0.8, 1.0]]
+        X1 = np.random.multivariate_normal(mean1, cov, 50)
+        X1 = np.vstack((X1, np.random.multivariate_normal(mean3, cov, 50)))
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 50)
+        X2 = np.vstack((X2, np.random.multivariate_normal(mean4, cov, 50)))
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def gen_lin_separable_overlap_data():
+        # generate training data in the 2-d case
+        mean1 = np.array([0, 2])
+        mean2 = np.array([2, 0])
+        cov = np.array([[1.5, 1.0], [1.0, 1.5]])
+        X1 = np.random.multivariate_normal(mean1, cov, 100)
+        y1 = np.ones(len(X1))
+        X2 = np.random.multivariate_normal(mean2, cov, 100)
+        y2 = np.ones(len(X2)) * -1
+        return X1, y1, X2, y2
+
+    def split_train(X1, y1, X2, y2):
+        X1_train = X1[:90]
+        y1_train = y1[:90]
+        X2_train = X2[:90]
+        y2_train = y2[:90]
+        X_train = np.vstack((X1_train, X2_train))
+        y_train = np.hstack((y1_train, y2_train))
+        return X_train, y_train
+
+    def split_test(X1, y1, X2, y2):
+        X1_test = X1[90:]
+        y1_test = y1[90:]
+        X2_test = X2[90:]
+        y2_test = y2[90:]
+        X_test = np.vstack((X1_test, X2_test))
+        y_test = np.hstack((y1_test, y2_test))
+        return X_test, y_test
+
+
+    print "test linear"
+    test_linear()
+
+    print "test linear"
+    test_linear()
+
+    print "test non linear"
+    test_non_linear()
+    """
