de08656bd058df93eac9178affa7241034742ab7
Sung-Kook Kim <skkeem@gmail.com>
2015-12-16 12:07:29 +0900

    Divided the code into sections. 	modified:   one_seven.py

--- one_seven.py
+++ one_seven.py
@@ -1,10 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import input_data
+
+# Constants
 AREA = 200
 O = 'r'
 S = 'b'
 
+# Data Loading
+
+# Feature Extraction
+
+# Plotting
 x = np.asarray([0.2, 0.1, -1])
 y = np.asarray([-2, 0.7, 1.3])
 colors = [O, S, S]
