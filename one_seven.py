94e5d748cff58fa55d48f7c1cd69a810f4ed1c6f
Sung-Kook Kim <skkeem@gmail.com>
2015-12-16 12:57:55 +0900

    Cleanups 	modified:   one_seven.py

--- one_seven.py
+++ one_seven.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import input_data
 
 # Constants
-AREA = 200
+AREA = 50
+SIZE = 1000
 O = 'r'
 S = 'b'
 
@@ -25,7 +26,6 @@ def extract_1_7(images, labels):
             
     return array1, array7
 
-SIZE = 10000
 images = mnist.train.images[:SIZE]
 labels = mnist.train.labels[:SIZE]
 
@@ -45,8 +45,10 @@ def featureX(x):
     """
     return len(filter(lambda x: x>0, x))
 
-""" find the leftmost x which is not zero """
 def featureY(x):
+    """
+    Find the leftmost x which is not zero
+    """
     v = 28
 
     for j in range(0, 28):
@@ -55,7 +57,6 @@ def featureY(x):
 		v = i
 
     return v    
-    #pass
 
 x1 = map(featureX, array1)
 y1 = map(featureY, array1)
@@ -63,11 +64,6 @@ x7 = map(featureX, array7)
 y7 = map(featureY, array7)
 
 # Plotting
-#x1 = np.asarray([0.2, 0.1, -1])
-#y1 = np.asarray([-2, 0.7, 1.3])
-#x7 = np.asarray([0.5, 2.1, -4])
-#y7 = np.asarray([2, -0.7, 2.1])
-
 plt.scatter(x1, y1, s=AREA, c=O)
 plt.scatter(x7, y7, s=AREA, c=S)
 plt.show()
