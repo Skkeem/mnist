02af5862cc605b0cf8d6d0c9644e0389319f4ab7
Sung-Kook Kim <skkeem@gmail.com>
2015-12-16 12:17:19 +0900

    Data loading complete.

--- one_seven.py
+++ one_seven.py
@@ -9,13 +9,37 @@ O = 'r'
 S = 'b'
 
 # Data Loading
+mnist = input_data.read_data_sets("./data")
+
+print 'Reading data set complete'
+
+def extract_1_7(images, labels):
+    array1 = []
+    array7 = []
+
+    for i in range(len(labels)):
+        if labels[i] == 1:
+            array1.append(images[i])
+        elif labels[i] == 7:
+            array7.append(images[i])
+            
+    return array1, array7
+
+SIZE = 10000
+images = mnist.train.images[:SIZE]
+labels = mnist.train.labels[:SIZE]
+
+array0, array7 = extract_1_7(images, labels)
 
 # Feature Extraction
 
 # Plotting
-x = np.asarray([0.2, 0.1, -1])
-y = np.asarray([-2, 0.7, 1.3])
+x1 = np.asarray([0.2, 0.1, -1])
+y1 = np.asarray([-2, 0.7, 1.3])
+x7 = np.asarray([0.5, 2.1, -4])
+y7 = np.asarray([2, -0.7, 2.1])
 colors = [O, S, S]
 
-plt.scatter(x, y, s=AREA, c=colors)
+plt.scatter(x1, y1, s=AREA, c=O)
+plt.scatter(x7, y7, s=AREA, c=S)
 plt.show()
